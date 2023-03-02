1a39e4f9bc198457c959efec04681449ea9f5272
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-03-02 09:43:03 -0300

    Funcionando

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 //(() => {
-import BotaoConcluir from "./componentes/concluiTarefa.js";
-import BotaoDeleta from "./componentes/deletaTaref.js";
-
-const criarTarefa = (evento) => {
-
-    evento.preventDefault(); // não carrega a pagina, previni o comportamento da página de enviar os dados pra algum lugar
+    import BotaoConclui from './componentes/concluiTarefa.js'
+    import BotaoDeleta from './componentes/deletaTarefa.js'
+     
+        const criarTarefa = (evento) => {
     
-    const lista = document.querySelector('[data-list');
-    const input = document.querySelector ('[data-form-input]');     
-    const valor = input.value;
-
-    const tarefa = document.createElement ('li');
-    tarefa.classList.add('task');
-
-    const conteudo = `<p class="conteudo">${valor}</p>`
-
-    lista.appendChild(tarefa);
-    tarefa.innerHTML = conteudo;
-    tarefa.appendChild(BotaoConcluir());
-    tarefa.appendChild(BotaoDeleta());
-
-    input.value = " ";
-}
-
-const novaTarefa = document.querySelector('[data-form-button]');
-novaTarefa.addEventListener('click', criarTarefa);
-
+        evento.preventDefault()
+    
+        const lista = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
+    
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
+        const conteudo = `<p class="content">${valor}</p>`
+    
+        tarefa.innerHTML = conteudo
+    
+        tarefa.appendChild(BotaoConclui())
+        tarefa.appendChild(BotaoDeleta())
+        lista.appendChild(tarefa)
+        input.value = " "
+    
+    }
+    
+    const novaTarefa = document.querySelector('[data-form-button]')
+    
+    novaTarefa.addEventListener('click', criarTarefa)
 //}) ()
+
+
+
 
